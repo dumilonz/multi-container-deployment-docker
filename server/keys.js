96373d026b191c4c52622965b162adf7fe6d358c
Dumilonze <elkevndrwlt@gmail.com>
2020-04-21 18:31:02 +0700

    Initial commit

---
+++ server/keys.js
@@ -0,0 +1,10 @@
+// Environemnt variables. You have to set them up somewhere
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT
+};
